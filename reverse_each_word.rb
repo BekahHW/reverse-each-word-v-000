d47868714c5d6394eb910eac5c953b1656dcc6c7
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-09 01:08:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,28 +1,7 @@
-# def reverse_each_word(sentence)
-#   sentence.each do |word| 
-#     "#{word}"
-# end   
-# end
-
-# >> "1,2,3,4".split(",").map { |s| s.to_i }
-
-
-# def reverse_each_word(sentence)
-#   new_array = sentence.split(" ")
-#   "#{new_array.reverse}"
-#   new_array.collect do |word| 
-#     "#{word}"
-# end
-# end
 
 
 def reverse_each_word(sentence)
  new_array = sentence.split().collect(&:reverse!)
  new_array.join(" ")
 end
- 
-# def hamburger(toppings)
-# toppings.collect do |topping|
-#   puts "I love #{topping} on my burgers!"
-# end
-# end
+
