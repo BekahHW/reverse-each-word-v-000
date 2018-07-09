74d2f8467995008933181014cf1a92631b1f082e
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-09 00:54:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,9 @@
 
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
+  "#{new_array.reverse}"
   new_array.collect do |word| 
-  reverse_array = "#{word.reverse}"
+    "#{word}"
 end
 end
 
